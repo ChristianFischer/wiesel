eac5cb843f8484c4a89971d9a8f84db288b4514c
Christian Fischer <ChristianFischer@users.noreply.github.com>
2012-04-18 22:40:38 +0200

    added LGPLv3 licence text

--- src/common/wiesel/application.cpp
+++ src/common/wiesel/application.cpp
@@ -1,10 +1,24 @@
-/*
- * application.cpp
+/**
+ * Copyright (C) 2012
+ * Christian Fischer
  *
- *  Created on: 19.03.2012
- *      Author: Baldur
+ * https://bitbucket.org/baldur/wiesel/
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA
  */
-
 #include "application.h"
 
 using namespace wiesel;
--- src/common/wiesel/io/filesystem.h
+++ src/common/wiesel/io/filesystem.h
@@ -1,12 +1,26 @@
-/*
- * filesystem.h
+/**
+ * Copyright (C) 2012
+ * Christian Fischer
  *
- *  Created on: 09.04.2012
- *      Author: Baldur
+ * https://bitbucket.org/baldur/wiesel/
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA
  */
-
-#ifndef FILESYSTEM_H_
-#define FILESYSTEM_H_
+#ifndef __WIESEL_IO_FILESYSTEM_H__
+#define __WIESEL_IO_FILESYSTEM_H__
 
 #include "directory.h"
 #include "file.h"
@@ -54,4 +68,4 @@ namespace wiesel {
 	};
 
 } /* namespace wiesel */
-#endif /* FILESYSTEM_H_ */
+#endif /* __WIESEL_IO_FILESYSTEM_H__ */
