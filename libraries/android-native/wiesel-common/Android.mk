181c3c8af9ebaa6fe3a52847dd0e81786a0f37e6
Christian Fischer <ChristianFischer@users.noreply.github.com>
2012-07-25 01:16:39 +0200

    configured libxml2 for android build

--- libraries/android-native/wiesel-common/Android.mk
+++ libraries/android-native/wiesel-common/Android.mk
@@ -19,7 +19,7 @@ LOCAL_MODULE               := wiesel-common
 LOCAL_MODULE_FILENAME      := libwieselcommon
 LOCAL_CFLAGS               := -Werror
 LOCAL_LDLIBS               := 
-LOCAL_STATIC_LIBRARIES     := wiesel-base libpng-static android_native_app_glue
+LOCAL_STATIC_LIBRARIES     := wiesel-base libpng-static libxml2-static android_native_app_glue
 LOCAL_SRC_FILES            := $(MY_FILES)
 LOCAL_C_INCLUDES           := $(MY_LOCAL_FOLDERS)
 LOCAL_EXPORT_C_INCLUDES    := $(MY_LOCAL_FOLDERS)
@@ -30,6 +30,7 @@ include $(BUILD_STATIC_LIBRARY)
 
 # External third-party modules
 $(call import-module,third-party/libpng)
+$(call import-module,third-party/libxml)
 
 # NativeActivity stuff
 $(call import-module,android/native_app_glue)
