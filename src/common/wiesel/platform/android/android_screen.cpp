ad950e2a881c3cb95a9f96d1ed8cd492e20fb8ae
Christian Fischer <ChristianFischer@users.noreply.github.com>
2012-07-24 19:22:19 +0200

    enabled alpha blending on all existing engines

--- src/common/wiesel/platform/android/android_screen.cpp
+++ src/common/wiesel/platform/android/android_screen.cpp
@@ -157,6 +157,9 @@ bool AndroidScreen::initContext() {
 	glDisable(GL_CULL_FACE);
 	glDisable(GL_DEPTH_TEST);
 
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
 	// initialize the window's size
 	resize();
 
