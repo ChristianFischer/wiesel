2613f9d1a69f6a33a57e97c407c7f899403c768d
baldur <baldur@localhost>
2013-05-20 16:57:13 +0200

    moved sample code into seperate repository

--- samples/HelloWiesel/android/jni/Application.mk
+++
@@ -1,6 +0,0 @@
-# Build both ARMv5TE and ARMv7-A and x86 machine code.
-APP_PLATFORM := android-10
-#APP_ABI := armeabi armeabi-v7a x86
-APP_CPPFLAGS += -frtti
-APP_STL := gnustl_static
-APP_DEBUG := true
